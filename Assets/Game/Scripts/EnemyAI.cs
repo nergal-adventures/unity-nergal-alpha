2204a074a4068b8b62a3278364373539bbad9849
Roy Rodriguez <roy.rodriguez@avantica.com>
2021-02-07 15:58:45 -0400

    Enemy prefab and enemy AI completed

---
+++ Assets/Game/Scripts/EnemyAI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    //Speed
+    [SerializeField] private float _speed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Move Down
+        transform.Translate(Vector3.down * (_speed * Time.deltaTime));
+
+        // When off the screen on the bottom
+        if (transform.position.y < -7.0f)
+        {
+            // respawn back on top with a nex x positions between bounds of the screen.
+            var randomX = Random.Range(-9.0f, 9.0f);
+            transform.position = new Vector3(randomX, 7.0f, 0);
+        }
+    }
+}
