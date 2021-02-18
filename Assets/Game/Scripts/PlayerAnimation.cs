07a46c1e90b1919ced826ea7f06e1be92d69e712
Roy Rodriguez <roy.rodriguez@avantica.com>
2021-02-18 18:02:09 -0400

    Joystick Added

--- Assets/Game/Scripts/PlayerAnimation.cs
+++ Assets/Game/Scripts/PlayerAnimation.cs
@@ -7,31 +7,39 @@ public class PlayerAnimation : MonoBehaviour
     // Start is called before the first frame update
 
     private Animator _animator;
+    private Joystick _joystick;
+
     void Start()
     {
         _animator = GetComponent<Animator>();
+        _joystick = FindObjectOfType<Joystick>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        var joystickLeft = _joystick.Horizontal < 0 ? true : false;
+        var joystickRight = _joystick.Horizontal > 0 ? true : false;
+        var joystickIdle = _joystick.Horizontal == 0 ? true : false;
+
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow) || joystickLeft)
         {
             _animator.SetBool("Turn_Left", true);
             _animator.SetBool("Turn_Right", false);
         }
-        else if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.LeftArrow))
+        else if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.LeftArrow) || joystickIdle)
         {
             _animator.SetBool("Turn_Left", false);
             _animator.SetBool("Turn_Right", false);
         }
-        
-        
-        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+
+
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow) || joystickRight)
         {
             _animator.SetBool("Turn_Right", true);
             _animator.SetBool("Turn_Left", false);
-        } else if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.RightArrow))
+        }
+        else if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.RightArrow) || joystickIdle)
         {
             _animator.SetBool("Turn_Left", false);
             _animator.SetBool("Turn_Right", false);
